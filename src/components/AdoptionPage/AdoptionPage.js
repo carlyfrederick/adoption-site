472462d4ed5218891395175ec1d95d9dd07de315
Carly Frederick <carly@swoopsrch.com>
2018-02-18 16:18:32 -0600

    moving event listener to adoption page container

--- src/components/AdoptionPage/AdoptionPage.js
+++ src/components/AdoptionPage/AdoptionPage.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 import * as formActions from '../../actions/formActions';
+import * as sizingActions from '../../actions/sizingActions';
 import Header from './Header';
 import Body from './Body';
 import Form from './Form';
@@ -14,104 +15,131 @@ import PawPrint from '../assets/grey-paw-print.png';
 import '../App.css';
 
 class AdoptionPage extends Component {
-  render() {
-    const { windowWidth } = this.props;
-    const smallWindow = windowWidth === 'small';
-    return (<div className={'adoption-page'}>
-        <div className={'wrapper'}>
-            <div style={{
-                height: 30,
-                width: 'calc(100% - 20px)',
-                padding: '30px 10px',
-                display: 'inline-block',
-                textAlign: 'center',
-            }}>
-                {
-                    !smallWindow ?
-                        [
-                            <img
-                            key={'left-1'}
-                            style={{margin: '0px 5px'}}
-                            src={PawPrint}
-                            alt={'paw-print'}
-                            height={15}
-                            width={15}
-                            />,
-                            <img
-                            key={'left-2'}
-                            style={{margin: '0px 5px'}}
-                            src={PawPrint}
-                            alt={'paw-print'}
-                            height={20}
-                            width={20}
-                            />,
-                            <img
-                            key={'left-3'}
-                            style={{margin: '0px 5px'}}
-                            src={PawPrint}
-                            alt={'paw-print'}
-                            height={30}
-                            width={30}
-                            />
-                        ] : null
-                }
-                <div
-                style={{
+    constructor(props) {
+        super(props);
+        this.handleWindowResize = this.handleWindowResize.bind(this);
+    }
+
+    componentDidMount() {
+        if (window) {
+            window.addEventListener('resize', this.handleWindowResize);
+        }
+    }
+
+    componentWillUnmount() {
+        if (window) {
+            window.removeEventListener('resize', this.handleWindowResize)
+        }
+    }
+
+    handleWindowResize() {
+        if (window.innerWidth <= 700
+            && this.props.windowWidth === 'large') {
+            this.props.sizingActions.handleResize('small');
+        } else if (window.innerWidth > 700
+            && this.props.windowWidth === 'small') {
+            this.props.sizingActions.handleResize('large');
+        }
+    }
+
+    render() {
+        const { windowWidth } = this.props;
+        const smallWindow = windowWidth === 'small';
+        return (<div className={'adoption-page'}>
+            <div className={'wrapper'}>
+                <div style={{
+                    height: 30,
+                    width: 'calc(100% - 20px)',
+                    padding: '30px 10px',
                     display: 'inline-block',
-                    lineHeight: '30px',
-                    letterSpacing: '3px',
-                    fontSize: '30px',
-                    margin: '0px 5px'
+                    textAlign: 'center',
                 }}>
-                    THE ADOPTION PROJECT
+                    {
+                        !smallWindow ?
+                            [
+                                <img
+                                key={'left-1'}
+                                style={{margin: '0px 5px'}}
+                                src={PawPrint}
+                                alt={'paw-print'}
+                                height={15}
+                                width={15}
+                                />,
+                                <img
+                                key={'left-2'}
+                                style={{margin: '0px 5px'}}
+                                src={PawPrint}
+                                alt={'paw-print'}
+                                height={20}
+                                width={20}
+                                />,
+                                <img
+                                key={'left-3'}
+                                style={{margin: '0px 5px'}}
+                                src={PawPrint}
+                                alt={'paw-print'}
+                                height={30}
+                                width={30}
+                                />
+                            ] : null
+                    }
+                    <div
+                    style={{
+                        display: 'inline-block',
+                        lineHeight: '30px',
+                        letterSpacing: '3px',
+                        fontSize: '30px',
+                        margin: '0px 5px'
+                    }}>
+                        THE ADOPTION PROJECT
+                    </div>
+                    {
+                        !smallWindow ?
+                            [
+                                <img
+                                key={'right-1'}
+                                style={{margin: '0px 5px'}}
+                                src={PawPrint}
+                                alt={'paw-print'}
+                                height={30}
+                                width={30}
+                                />,
+                                <img
+                                key={'right-2'}
+                                style={{margin: '0px 5px'}}
+                                src={PawPrint}
+                                alt={'paw-print'}
+                                height={20}
+                                width={20}
+                                />,
+                                <img
+                                key={'right-3'}
+                                style={{margin: '0px 5px'}}
+                                src={PawPrint}
+                                alt={'paw-print'}
+                                height={15}
+                                width={15}
+                                />
+                            ] : null
+                    }
                 </div>
+                <Header />
+                <Body />
+                <ApplicationButton {...this.props}/>
+                <Form {...this.props}/>
                 {
-                    !smallWindow ?
-                        [
-                            <img
-                            key={'right-1'}
-                            style={{margin: '0px 5px'}}
-                            src={PawPrint}
-                            alt={'paw-print'}
-                            height={30}
-                            width={30}
-                            />,
-                            <img
-                            key={'right-2'}
-                            style={{margin: '0px 5px'}}
-                            src={PawPrint}
-                            alt={'paw-print'}
-                            height={20}
-                            width={20}
-                            />,
-                            <img
-                            key={'right-3'}
-                            style={{margin: '0px 5px'}}
-                            src={PawPrint}
-                            alt={'paw-print'}
-                            height={15}
-                            width={15}
-                            />
-                        ] : null
+                    this.props.showConfirmation ?
+                    <ConfirmationDialog {...this.props}/> : null
                 }
+                <BackToHomepage />
             </div>
-            <Header />
-            <Body />
-            <ApplicationButton {...this.props}/>
-            <Form {...this.props}/>
-            {
-                this.props.showConfirmation ?
-                <ConfirmationDialog {...this.props}/> : null
-            }
-            <BackToHomepage />
-        </div>
-      </div>);
-  }
+          </div>);
+    }
 }
 
 AdoptionPage.propTypes = {
     formActions: PropTypes.object,
-    form: PropTypes.array
+    form: PropTypes.array,
 };
 
 function mapStateToProps(state) {
@@ -124,7 +152,8 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        formActions: bindActionCreators(formActions, dispatch)
+        formActions: bindActionCreators(formActions, dispatch),
+        sizingActions: bindActionCreators(sizingActions, dispatch)
     };
 }
 
